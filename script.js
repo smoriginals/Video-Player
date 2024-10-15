86e16092446ad08036ef7ceb8fc7262fa55813a2
SM ORIGINALS <SMOS-Github@users.noreply.github.com>
2024-10-15 21:56:02 +0530

    Custom Video Player
    -on screen play/pause.
    -on button play/pause.
    -mute/increase/descrease sound
    -video duration increase/descrease by slider
    -video playback rate cantrol
    -10/+15 sec fast forward
    -speed2x/0.5x
    -double click on speed to make normal speed
    custom hud.

--- script.js
+++ script.js
@@ -1,120 +1,87 @@
-const play = document.querySelector(".play-button");
-const myVideo = document.querySelector("video");
+const pslider = document.getElementById("playback-slider");
+const sslider = document.getElementById("sound-slider");
+const video = document.querySelector("video");
 const volume = document.querySelector(".volume-button");
-const seekF = document.querySelector(".seek15-button");
-const seekB = document.querySelector(".seek10-button");
-const fullS = document.querySelector(".full-button");
-let vidSeek = document.getElementById("vid-basis");
+const playing = document.querySelector(".play-button");
+const seekback = document.querySelector(".seek-backward");
+const seekforward = document.querySelector(".seek-forward");
+const speedup = document.querySelector(".speed-up");
+const speedown = document.querySelector(".speed-down");
 
-let isPlay = false;
-let hasClick = false;
-let hasAudio = true;
-let seekFW = false;
-let seekBW = false;
-let fw = 15;
-let bw = 10;
+let onVideoClick = false;
+let onVolumeClick = true;
+let onClickPlay = false;
 
 
-const ControlVolume = volume.addEventListener("click", () => {
-    
-    if (hasAudio) {
-        volume.classList.add("volume-button-update");
-        myVideo.volume = 0;
-    }
-    else {
-        volume.classList.remove("volume-button-update");
-        myVideo.volume = 1;
-    }
-    hasAudio = !hasAudio;
-});
-
-
-const PlayPause = play.addEventListener("click", () => {
-    isPlay = true;
-    hasClick = !hasClick;
-    if (isPlay && hasClick) {
-        myVideo.play();
-        play.classList.add("play-button-update");
-    }
-    else {
-        myVideo.pause();
-        play.classList.remove("play-button-update");
-    }
-});
-
-const FastFoward = seekF.addEventListener("click", () => {
-    seekFW = !seekFW;
-    if (seekFW) {
-        seekF.classList.add("seek15-button-update");
-        myVideo.currentTime += fw;
+//Video play by his background click
+video.addEventListener("click", () => {
+    onVideoClick = !onVideoClick;
+    if (onVideoClick) {
+        video.play();
     } else {
-        seekF.classList.remove("seek15-button-update");
+        video.pause();
     }
+});
+
+//volume control
+volume.addEventListener("click", () => {
     
-});
-const BackFoward = seekB.addEventListener("click", () => {
-    seekBW = !seekBW;
-    if (seekBW) {
-        seekB.classList.add("seek10-button-update");
-        myVideo.currentTime -= bw;
+    if (onVolumeClick) {
+        video.volume = 0;
+        volume.classList.add("muted");
     } else {
-        seekB.classList.remove("seek10-button-update");
+        video.volume = 1;
+        volume.classList.remove("muted");
     }
-
+    onVolumeClick = !onVolumeClick;
 });
 
-const fullScreen = fullS.addEventListener("click", () => {
-    myVideo.requestFullscreen();
+//play with button
+playing.addEventListener("click", () => {
+    onClickPlay = !onClickPlay;
+    if (onClickPlay) {
+        video.play();
+        playing.classList.add("pause");
+    } else {
+        video.pause();
+        playing.classList.remove("pause");
+    }
 });
 
-myVideo.addEventListener("timeupdate", () => {
-    const deltaTime = 100;
-    const progress = (myVideo.currentTime / myVideo.duration) * deltaTime;
-    vidSeek.style.width = `${progress}%`;
+//seeking video
+seekback.addEventListener("click", () => {
+    video.currentTime = Math.max(0, video.currentTime - 10.0);
+});
+seekforward.addEventListener("click", () => {
+    video.currentTime = Math.max(video.duration, video.currentTime + 15.0);
 });
 
+//speeding video
+speedup.addEventListener("click", () => {
+    video.playbackRate = 2;
+});
+speedup.addEventListener("dblclick", () => {
+    video.playbackRate = 1;
+});
+speedown.addEventListener("click", () => {
+    video.playbackRate = 0.5;
+});
+speedown.addEventListener("dblclick", () => {
+    video.playbackRate = 1;
+});
 
-fullScreen();
-BackFoward();
-FastFoward();
-ControlVolume();
-PlayPause();
+//cantrol durition progress
 
+video.addEventListener("timeupdate", () => {
+    const deltaTime = 100.0;
+    const progress = (video.currentTime / video.duration) * deltaTime;
+    pslider.value = `${progress}`;
+});
+pslider.addEventListener('input', (e) => {
+    const newTime = (e.target.value / 100) * video.duration;
+    video.currentTime = newTime;
+});
 
-/**<script>
-    const video = document.getElementById('video');
-    const progressDiv = document.getElementById('progress-div');
-    const videoContainer = document.getElementById('video-container');
-
-    // Update the width of the progress div as video plays
-    video.addEventListener('timeupdate', () => {
-        const progress = (video.currentTime / video.duration) * 100;
-        progressDiv.style.width = `${progress}%`;
-    });
-
-    // Add drag functionality to fast-forward/rewind the video
-    progressDiv.addEventListener('mousedown', (e) => {
-        function seekVideo(e) {
-            const rect = videoContainer.getBoundingClientRect();
-            const position = e.clientX - rect.left;
-            const percentage = position / rect.width;
-            video.currentTime = percentage * video.duration;
-        }
-
-        function stopDrag() {
-            document.removeEventListener('mousemove', seekVideo);
-            document.removeEventListener('mouseup', stopDrag);
-        }
-
-        document.addEventListener('mousemove', seekVideo);
-        document.addEventListener('mouseup', stopDrag);
-    });
-
-    // Sync video length with the div on load
-    video.addEventListener('loadedmetadata', () => {
-        progressDiv.style.width = '0%';
-    });
-</script>
-
-</body>
-</html> */
+sslider.addEventListener('input', (e) => {
+    video.volume = e.target.value;
+});
